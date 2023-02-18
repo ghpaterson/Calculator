844aca29939900901bdadc0675ddd8c5fa006e1a
Graeme Paterson <ghpaterson@gmail.com>
2023-02-18 11:09:35 +0000

    created calculator class and defined some functions

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+class Calculator {
+  constructor(previousOperandTextElement, currentOperandTextElement) {
+    this.previousOperandTextElement = previousOperandTextElement;
+    this.currentOperandTextElement = currentOperandTextElement;
+    this.clear();
+  }
+
+  clear() {
+    this.currentOperand = "";
+    this.previousOperand = "";
+    this.operation = undefined;
+  }
+
+  delete() {}
+
+  appendNumber(number) {
+    this.currentOperand = number;
+  }
+
+  chooseOperation(operation) {}
+
+  calculate() {}
+
+  updateDisplay() {
+    this.currentOperandTextElement.innerText = this.currentOperand;
+  }
+}
+
 const numberButtons = document.querySelectorAll("[data-number]");
 const operationButtons = document.querySelectorAll("[data-operation");
 const equalsButton = document.querySelector("[data-equals]");
@@ -9,3 +37,15 @@ const previousOperandTextElement = document.querySelector(
 const currentOperandTextElement = document.querySelector(
   "[data-current-operand]"
 );
+
+const calculator = new Calculator(
+  previousOperandTextElement,
+  currentOperandTextElement
+);
+
+numberButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    calculator.appendNumber(button.innerText);
+    calculator.updateDisplay();
+  });
+});
